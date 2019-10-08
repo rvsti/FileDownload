39e2dd7f69ea48a8a4e890dae9e5acb3f8c06929
mmt7924 <Raj.singh@go-mmt.com>
2019-10-08 17:19:14 +0530

    Adding test

--- src/main/java/svc/impl/HttpDownloader.java
+++ src/main/java/svc/impl/HttpDownloader.java
@@ -12,9 +12,8 @@ public class HttpDownloader implements Downloader {
   @Override
   public void downloadFile(String urlStr, String localPath) throws IOException {
     URL url = new URL(urlStr);
-    String file = AppConstants.Http_Filepath;
     BufferedInputStream bis = new BufferedInputStream(url.openStream());
-    FileOutputStream fis = new FileOutputStream(file);
+    FileOutputStream fis = new FileOutputStream(localPath);
     byte[] buffer = new byte[1024];
     int count=0;
     while((count = bis.read(buffer,0,1024)) != -1)
